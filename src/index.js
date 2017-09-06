369b8daafa3be9112a1ed72b1a2d483d282cefae
Koji Aomatsu <penbook.koji@gmail.com>
2017-09-07 00:31:49 +0900

    Add basic component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Label extends React.Component {
+  render(){
+    if (this.props.thisIsTheFirst) {
+      return (
+          <p>Please press the button</p>
+        );
+    } else {
+      let res = [];
+      let nums = [4,5,6];
+      for (var i = 0; i < nums.length; i++) {
+        res.push(<li>{nums[i]}</li>);
+      }
+      return (
+          <ul>{res}</ul>
+        );
+    }
+  }
+}
+
+class Form extends React.Component {
+  render() {
+    return (
+      <button className="shuffle" onClick={() => this.props.onClick()}>Shuffle</button>
+    );
+  }
+}
+
+class History extends React.Component {
+  render() {
+    return(
+        <p>history</p>
+    );
+  }
+}
+
+class MainApp extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      thisIsTheFirst : true,
+      numlist : [[8],[9,8]],
+      amount : 2,
+      maximum : 2000
+    }
+  }
+  render(){
+    return (
+      <div>
+        <Label
+          thisIsTheFirst = {this.thisIsTheFirst}
+          numlist = {this.numlist}
+        />
+        <Form
+          onClick={() => this.handleClick()}
+        />
+        <History />
+      </div>
+    );
+  }
+  handleClick() {
+  }
+}
+
+ReactDOM.render(<MainApp />, document.getElementById('root'));
 registerServiceWorker();
